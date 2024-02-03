8e11407b9f1717dfc833bd6470b52b9414ecfd05
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2024-02-03 22:07:16 +0900

    スレ一覧ページをリファクタリング

--- lib/thread_list_page.dart
+++ lib/thread_list_page.dart
@@ -1,9 +1,9 @@
+import 'dart:io';
+import 'package:charset_converter/charset_converter.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:charset_converter/charset_converter.dart';
-import 'horizontal_drag_mixin.dart'; // Import the mixin
+import 'horizontal_drag_mixin.dart';
 import 'common.dart';
-import 'dart:io';
 
 class ThreadListPage extends StatefulWidget {
   final String url;
@@ -29,17 +29,14 @@ class ThreadListPageState extends State<ThreadListPage> with HorizontalDragMixin
       return 'HTMLの取得に失敗しました';
     }
 
-    String? charset = response.headers['content-type']?.split('charset=')[1];
-    charset ??= Common.defaultCharset;
-    if (charset == Common.shiftJisCharset) {
-      charset = Common.windowsCharset;
-    }
+    String charset = response.headers['content-type']?.split('charset=')[1] ?? Common.defaultCharset;
+    charset = (charset == Common.shiftJisCharset) ? Common.windowsCharset : charset;
 
     try {
       final decodedBody = await CharsetConverter.decode(charset, response.bodyBytes);
       return decodedBody;
     } catch (e) {
-      return 'デコード中にエラーが発生しました: $e';
+      return 'デコード中にエラーが発生しました。詳細: $e';
     }
   }
 
@@ -55,20 +52,23 @@ class ThreadListPageState extends State<ThreadListPage> with HorizontalDragMixin
         ),
         body: FutureBuilder<String>(
           future: _htmlContent,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              return SingleChildScrollView(
-                child: Text(snapshot.data ?? 'No data'),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
+          builder: _buildHtmlContent,
         ),
       ),
     );
+  }
+
+  Widget _buildHtmlContent(BuildContext context, AsyncSnapshot<String> snapshot) {
+    if (snapshot.connectionState == ConnectionState.done) {
+      return snapshot.hasError
+          ? Text('エラーが発生しました。詳細: ${snapshot.error}')
+          : _buildHtmlScrollView(snapshot.data ?? 'データがありません。');
+    } else {
+      return const CircularProgressIndicator();
+    }
+  }
+
+  Widget _buildHtmlScrollView(String data) {
+    return SingleChildScrollView(child: Text(data));
   }
 }
